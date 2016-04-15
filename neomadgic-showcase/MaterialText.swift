8e0623d66911453ec334b69d324fcbe5e182a5d7
Vu Dang <jublusion5@gmail.com>
2016-04-14 23:41:49 -0500

    Created custom text field class, finish up main storyboard view

---
+++ neomadgic-showcase/MaterialText.swift
@@ -0,0 +1,33 @@
+//
+//  MaterialText.swift
+//  neomadgic-showcase
+//
+//  Created by Vu Dang on 4/14/16.
+//  Copyright © 2016 Vu Dang. All rights reserved.
+//
+
+import UIKit
+
+class MaterialText: UITextField
+{
+    
+    override func awakeFromNib()
+    {
+        layer.cornerRadius = 2.0
+        layer.borderColor = UIColor(red: SHADOW_COLOR, green: SHADOW_COLOR, blue: SHADOW_COLOR, alpha: 0.1).CGColor
+        layer.borderWidth = 1.0
+
+    }
+    
+    override func textRectForBounds(bounds: CGRect) -> CGRect
+    {
+        return CGRectInset(bounds, 10, 0)
+    }
+    
+    override func editingRectForBounds(bounds: CGRect) -> CGRect
+    {
+        return CGRectInset(bounds, 10, 0)
+    }
+   
+
+}
