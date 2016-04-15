d4369cb366bdf03d30213a4a491a6ef03c973dc9
Vu Dang <jublusion5@gmail.com>
2016-04-15 11:24:00 -0500

    Installed Firebase pod

---
+++ Pods/Target Support Files/Pods-neomadgic-showcase/Pods-neomadgic-showcase-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_neomadgic_showcase : NSObject
+@end
+@implementation PodsDummy_Pods_neomadgic_showcase
+@end
---
+++ Pods/Target Support Files/Pods-neomadgic-showcase/Pods-neomadgic-showcase-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_neomadgic_showcaseVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_neomadgic_showcaseVersionString[];
+
