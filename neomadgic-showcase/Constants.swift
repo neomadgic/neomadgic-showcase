259a2e46571681f17ee75c8d353f9584fd225242
Vu Dang <jublusion5@gmail.com>
2016-04-14 22:18:31 -0500

    Created custom class MaterialView and added constant SHADOW_COLOR

---
+++ neomadgic-showcase/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  neomadgic-showcase
+//
+//  Created by Vu Dang on 4/14/16.
+//  Copyright © 2016 Vu Dang. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+let SHADOW_COLOR: CGFloat = 157.0/255.0
