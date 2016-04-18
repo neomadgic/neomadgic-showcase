f1df3187aa27eeca6b310ae68ea1867d00d59b70
Vu Dang <jublusion5@gmail.com>
2016-04-18 02:07:07 -0500

    created an IBAction for facebook login button  and successfully log into facebook

--- neomadgic-showcase/ViewController.swift
+++ neomadgic-showcase/ViewController.swift
@@ -7,19 +7,42 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
 
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func facebookLoginPressed(sender: UIButton!)
+    {
+        let facebookLogin = FBSDKLoginManager()
+        
+        facebookLogin.logInWithReadPermissions(["email"], fromViewController: self)
+            { (facebookResult: FBSDKLoginManagerLoginResult!, facebookError: NSError!) in
+                
+                if facebookError != nil
+                    {
+                        print("Facebook login failed. Error \(facebookError)")
+                    }
+                else
+                    {
+                        let accessToken = FBSDKAccessToken.currentAccessToken().tokenString
+                        print("Successfully logged in with Facebook. \(accessToken)")
+                    }
+            }
+    }
 
 }
 
