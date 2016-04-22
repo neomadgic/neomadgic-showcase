f89bce1d285dc6d4c17d20509d0c318a7f1974ed
Vu Dang <jublusion5@gmail.com>
2016-04-22 13:01:48 -0500

    Created FeedVC and prepped it for tableview

---
+++ neomadgic-showcase/FeedVC.swift
@@ -0,0 +1,40 @@
+//
+//  FeedVC.swift
+//  neomadgic-showcase
+//
+//  Created by Vu Dang on 4/22/16.
+//  Copyright © 2016 Vu Dang. All rights reserved.
+//
+
+import UIKit
+
+class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource
+{
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad()
+    {
+        tableView.delegate = self
+        tableView.dataSource = self
+        super.viewDidLoad()
+    }
+    
+    
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int
+    {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return 3
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
+    {
+        return UITableViewCell()
+    }
+    
+}
